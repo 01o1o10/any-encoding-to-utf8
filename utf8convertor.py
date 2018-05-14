44bb456cece2b4f1c972f8a639894766b94ffa15
ilyas.mammadov <by.computer.engineer@gmail.com>
2018-05-14 21:56:34 +0300

    version 1.1.0

--- utf8convertor.py
+++ utf8convertor.py
@@ -6,21 +6,50 @@ from tkinter import filedialog
 from tkinter import *
  
 root = Tk()
-root.directory =  filedialog.askdirectory(initialdir = "/home/ilyas/Downloads/",title = "Select Directory")
+root.directory =  filedialog.askdirectory(initialdir = "./",title = "Select Directory")
+
+i = 0
 
 def edit(src):
+	global i
 	if os.path.isfile(src):
-		doc = codecs.open(src, 'r', 'windows-1254')
-		docContent = doc.read()
-		doc.close()
-		os.remove(src)
-		newDoc = open(src, 'w')
-		newDoc.write(docContent)
-		newDoc.close()
-		print('Edited: ', src)
+		doc = open(src, encoding='utf-8')
+		docContent = ""
+		try:
+			docContent = doc.read()
+			os.remove(src)
+			newDoc = open(src, 'w')
+			newDoc.write(docContent)
+			newDoc.close()
+			print('Edited: ', src)
+		except: 
+			doc.close()
+			doc = open(src, encoding='windows-1254') 
+			try:
+				docContent = doc.read()
+				os.remove(src)
+				newDoc = open(src, 'w')
+				newDoc.write(docContent)
+				newDoc.close()
+				print('Edited: ', src)
+			except: 
+				doc.close()
+				doc = open(src, encoding='windows-1252')
+				try:
+					docContent = doc.read()
+					os.remove(src)
+					newDoc = open(src, 'w')
+					newDoc.write(docContent)
+					newDoc.close()
+					print('Edited: ', src)
+				except: 
+					i += 1
+					os.system('tput setaf 1; echo "\nFile path : ' + src + '"; echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! File can\'t read !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"; tput sgr0;')
 	else:
 		for dir in os.listdir(src):
 			print(src + '/' + dir)
 			edit(src + '/' + dir)
 
+
 edit(root.directory)
+os.system('tput setaf 1; echo ' + str(i) + '" File can\'t read!"')
